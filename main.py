60586d74ac4dd1aaae24604bf69876ee541a92fa
Alex Selby <alex.selby@hotmail.com>
2021-10-06 13:15:55 +0100

    onkey functions added for movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from turtle import Screen, Turtle
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -7,38 +8,19 @@ screen.bgcolor("black")
 screen.title("Snake")
 screen.tracer(0)
 
+snake = Snake()
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
+screen.listen()
+screen.onkey(key="Up", fun=snake.move_up)
+screen.onkey(key="Down", fun=snake.move_down)
+screen.onkey(key="Left", fun=snake.move_left)
+screen.onkey(key="Right", fun=snake.move_right)
 
-for position in starting_positions:
-    snake_body = Turtle(shape="square")
-    snake_body.pu()
-    snake_body.color("white")
-    snake_body.goto(position)
-    segments.append(snake_body)
-
-
-  
-game_is_on  = True
+game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     
-    for seg in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg - 1].xcor()
-        new_y = segments[seg - 1].ycor()
-        segments[seg].goto(new_x, new_y)
-        
-    segments[0].fd(20)
-        
-        
-
-
-
-
-
-
-
+    snake.move()
 
 screen.exitonclick()
