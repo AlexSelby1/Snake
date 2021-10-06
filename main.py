bfbf035f091a02815be6fd50d77fd7b693db7d92
Alex Selby <alex.selby@hotmail.com>
2021-10-06 11:54:25 +0100

    correcting snake body as it wasn't connected

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake")
+screen.tracer(0)
+
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
+
 for position in starting_positions:
-    snake = Turtle(shape="square")
-    snake.color("white")
-    snake.goto(position)
+    snake_body = Turtle(shape="square")
+    snake_body.pu()
+    snake_body.color("white")
+    snake_body.goto(position)
+    segments.append(snake_body)
 
+
+  
+game_is_on  = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     
-
-
+    for seg in range(len(segments) - 1, 0, -1):
+        new_x = segments[seg - 1].xcor()
+        new_y = segments[seg - 1].ycor()
+        segments[seg].goto(new_x, new_y)
+        
+    segments[0].fd(20)
+        
+        
 
 
 
